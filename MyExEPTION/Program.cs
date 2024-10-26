0b7292f17ba6dcb57d8b463412d48ca7f27e82bf
Murad <agamedov94@mail.ru>
2024-10-26 21:15:10 +0400

    commit

---
+++ MyExEPTION/Program.cs
@@ -0,0 +1,46 @@
+﻿using MyExEPTION;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] programmingLanguages = { "C#", "Java", "Python", "JavaScript", "Ruby", "Swift", "PHP", "Go", "Kotlin" };
+
+        Console.WriteLine("Here are some popular programming languages:");
+        foreach (string language in programmingLanguages)
+        {
+            Console.WriteLine(language);
+        }
+
+        bool languageFound = false;
+
+        while (!languageFound)
+        {
+            Console.WriteLine("\nWhat is the best programming language in the world?");
+            string userInput = Console.ReadLine();
+
+            foreach (string language in programmingLanguages)
+            {
+                if (userInput.Equals(language, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (language == "PHP")
+                    {
+                        Console.WriteLine("You are the most amazing person for choosing PHP!");
+                    }
+                    else
+                    {
+                        throw new ProgrammingLanguageNotFoundException();
+                    }
+                    languageFound = true;
+                    break;
+                }
+            }
+
+            if (!languageFound)
+            {
+                Console.WriteLine($"The programming language '{userInput}' is not in the list. But surely, you were thinking of PHP. Please try again.");
+            }
+        }
+    }
+}
---
+++ MyExEPTION/ProgrammingLanguageNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExEPTION
+{
+    public class ProgrammingLanguageNotFoundException : Exception
+    {
+        public ProgrammingLanguageNotFoundException()
+            : base("PHP is not included! You're right, PHP isn't the best language... It's the most amazing language!") { }
+    }
+}
